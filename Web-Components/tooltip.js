cbc92fe31c34f61fc59b0581fa066956412be71b
Anusha Medicharla <anusha.medicharla@pearson.com>
2020-01-08 11:21:54 -0700

    Adding host styling

--- Web-Components/tooltip.js
+++ Web-Components/tooltip.js
@@ -24,26 +24,82 @@ class Tooltip extends HTMLElement {
         this.shadowRoot.innerHTML = `
         <style>
             div {
+                font-weight: normal;
                 background-color: black;
                 color: white;
                 position : absolute;
+                top: 1.5rem;
+                left: 0.57rem;
                 z-index: 10;
+                padding: 0.15rem;
+                border-radius: 3px;
+                box-shadow: 1px 1px 6px rgba(0,0,0,0.26);
+            }
+
+            /*
+                We cannot use the selector of uc-tooltip here in order to select and style our entire element.
+                instead we have this 'host' selector to style this element. Of course this would act as a default 
+                styling and any styling in the real DOM for this element ( using the tag uc-tooltip ) would have more 
+                specificity and would override this styling.
+            */
+            :host {
+                background: #ccc;
+            }
+
+            /*
+                Lets say we have a class "important" attached to our host element. We cannot just use :host.important
+                type of selector to use it. Instead we use :host(.important) {} as the host also is a function that 
+                takes in a selector to add the styling conditionally to that particular selector.
+            */
+            :host(.important) {
+                /*  We use this variable using the var method here. If that value is not available, this fallback 
+                    value of #ccc that we mentioned here gets applied. */
+                background: var(--color-primary, #ccc);
+            }
+
+            /*
+                We can specify the  context in which the styling needs to be applied for this host with
+                :host-context method.           
+            */
+            :host-context(p.hello) {
+                font-weight: bold;
+            }
+
+            .icon {
+                background-color: black;
+                color: white;
+                padding: 0.15rem 0.5rem;
+                text-align: center;
+                border-radius: 50%;
+            }
+            .highlight {
+                background: red;
+            }
+            /*  In order to style the slotted content in the 
+                shadow DOM, we need to use ::slotted {} selector in the shadow DOM styling. 
+                We can only selected the top most slotted content . for example we cannot select an 'a' tag 
+                under this .highlight class. The styling in the real DOM overrides the styling in the shadow DOM.
+                So this styling could be a default set styling. 
+            */
+            ::slotted(.highlight) {
+                // border-bottom: 1px solid red;
             }
         </style>
             <slot>Some Default</slot>
-            <span> (?)</span>
+            <span class="icon">?</span>
         `;
     }
 
-    // Element gets attached to the DOM here.
+    /* Lifecycle Hook : At this point of time this element gets attached to the real DOM.
+    */
     connectedCallback() {
+
         // Fetch the attribute on this HTMLElement.
         if(this.hasAttribute('text')) {
             this._tooltipText = this.getAttribute('text');      
         }
         const tooltipIcon = this.shadowRoot.querySelector('span');
         // const tooltipIcon = document.createElement('span');
-        tooltipIcon.textContent = ' (?)';
         tooltipIcon.addEventListener('mouseenter', this._showTooltip.bind(this));
         tooltipIcon.addEventListener('mouseleave', this._hideTooltip.bind(this));
         /*  The new children are not attached to the actual(light) DOM, instead they are now attached
