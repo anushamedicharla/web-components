90cdae56746bec15e4318c1be0667dc69a8c2d5c
Anusha Medicharla <anusha.medicharla@pearson.com>
2020-01-08 12:24:29 -0700

    Clean up

--- Web-Components/tooltip.js
+++ Web-Components/tooltip.js
@@ -3,7 +3,9 @@ class Tooltip extends HTMLElement {
     constructor() {
         super();
         // Naming convention to replicate the behaviour of a private member of this class.
-        this._tooltipContainer;
+        // this._tooltipContainer;
+        this._tooltipIcon;
+        this._tooltipVisible = false;
         this._tooltipText = 'Dummy tooltip.';
 
         /*  Unlock Shadow Dom. A shadow dom tree is being attached to the HTMLElement. 
@@ -44,6 +46,7 @@ class Tooltip extends HTMLElement {
             */
             :host {
                 background: #ccc;
+                position: relative;
             }
 
             /*
@@ -98,17 +101,56 @@ class Tooltip extends HTMLElement {
         if(this.hasAttribute('text')) {
             this._tooltipText = this.getAttribute('text');      
         }
-        const tooltipIcon = this.shadowRoot.querySelector('span');
+        this._tooltipIcon = this.shadowRoot.querySelector('span');
         // const tooltipIcon = document.createElement('span');
-        tooltipIcon.addEventListener('mouseenter', this._showTooltip.bind(this));
-        tooltipIcon.addEventListener('mouseleave', this._hideTooltip.bind(this));
+        this._tooltipIcon.addEventListener('mouseenter', this._showTooltip.bind(this));
+        this._tooltipIcon.addEventListener('mouseleave', this._hideTooltip.bind(this));
         /*  The new children are not attached to the actual(light) DOM, instead they are now attached
             to the shadow ROOT. Visually it doesn't change much.
         */
-        // this.appendChild(tooltipIcon);
-        this.shadowRoot.appendChild(tooltipIcon);
-        this.style.position = 'relative';
+        // this.appendChild(_tooltipIcon);
+        this.shadowRoot.appendChild(this._tooltipIcon);
+        // this.style.position = 'relative';
     }
+
+    /*
+        Attribute changes do not get picked up because we have no logic for that in the component. The "text"
+        attribute is extracted in connectedCallback (i.e when the component gets mounted to the DOM) only.
+        For this we have a new lifecycle hook: attributeChangedCallback()
+        This gets executed when an Observed attribute is updated. Observed because this is a great optimization
+        technique in JS now. All attributes do not get watched automatically. 
+        We need to explicitly observe these attributes.
+    */
+
+    attributeChangedCallback(name, oldVal, newVal) {
+        console.log(name, oldVal, newVal);
+        if( oldVal === newVal) {
+            return;
+        }
+        if( name === 'text') {
+            // i.e for 'text' attribute changes
+            this._tooltipText = newVal;
+        }
+    }
+
+    /*
+        This will be called when the element will be removed from the real DOM.
+        A great place for any cleanup work.
+    */
+    disconnedtedCallback() {
+        console.log('Disconnedted');
+        this._tooltipIcon.removeEventListener('mouseenter', this._showTooltip);
+        this._tooltipIcon.removeEventListener('mouseleave', this._hideTooltip);
+    }
+
+    static get observedAttributes() {
+        /*  This is like a property of this class but its not. We cannot set this property. 
+            we can only get this and this is accessible from outside this class. Like a locked down 
+            property. We return the observed properties here.
+        */
+        return ['text'];
+    }
+
 
     // Naming convention to replicate the behaviour of a private member of this class.
     _showTooltip() {
@@ -121,17 +163,40 @@ class Tooltip extends HTMLElement {
             // this._tooltipContainer.style.position = 'absolute';
             // this._tooltipContainer.style.zIndex = '10';
         */
-       this._tooltipContainer = document.createElement('div');
-            this._tooltipContainer.textContent = this._tooltipText;
-            this._tooltipContainer.style.backgroundColor = 'black';
-            this._tooltipContainer.style.color = 'white';
-            this._tooltipContainer.style.position = 'absolute';
-            this._tooltipContainer.style.zIndex = '10';
-        this.shadowRoot.appendChild(this._tooltipContainer);
+        // this._tooltipContainer = document.createElement('div');
+        // this._tooltipContainer.textContent = this._tooltipText;
+            // this._tooltipContainer.style.backgroundColor = 'black';
+            // this._tooltipContainer.style.color = 'white';
+            // this._tooltipContainer.style.position = 'absolute';
+            // this._tooltipContainer.style.zIndex = '10';
+        // this.shadowRoot.appendChild(this._tooltipContainer);
+
+        this._tooltipVisible = true;
+        this._render();
     }
 
     _hideTooltip() {
-        this.shadowRoot.removeChild(this._tooltipContainer);
+        this._tooltipVisible = false;
+        this._render();
+        // this.shadowRoot.removeChild(this._tooltipContainer);
+    }
+
+
+    /* 
+        A private method responsible for updating the DOM.    
+        It is cleaner to add all the logic of rendering things to the DOM in one place.
+    */
+    _render() {
+        let tooltipContainer = this.shadowRoot.querySelector('div');
+        if( this._tooltipVisible) {
+            tooltipContainer = document.createElement('div');
+            tooltipContainer.textContent = this._tooltipText;
+            this.shadowRoot.appendChild(tooltipContainer);
+        } else {
+            if(tooltipContainer) {
+                 this.shadowRoot.removeChild(tooltipContainer);
+            }
+        }
     }
 }
 
